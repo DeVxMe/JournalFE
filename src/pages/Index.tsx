a5d1dca8958b238dbcdc4eced5abbf0ac703227f
DeVxMe <bimalcgalise123@gmail.com>
2025-09-13 12:04:38 +0545

    Pages

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { useWallet } from '@solana/wallet-adapter-react';
+import { LandingPage } from '../components/LandingPage';
+import { Dashboard } from '../components/Dashboard';
+
+const Index = () => {
+  const { connected } = useWallet();
+
+  return connected ? <Dashboard /> : <LandingPage />;
+};
+
+export default Index;
